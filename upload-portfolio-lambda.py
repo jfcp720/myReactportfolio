9512607c36f3b92a0e6811a4e5f24de523e98a8a
Jesus Colon <jfcp720@gmail.com>
2017-09-25 14:45:05 -0400

    update portfolio deployment function for lambda

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -1,25 +1,31 @@
+#import dependencies (libraries)
 import boto3
 import StringIO
 import zipfile
 import mimetypes
 
-#create names for the resource
-s3 = boto3.resource('s3')
+def lambda_handler(event, context):
 
-#create names for the buckets
-portfolio_bucket = s3.Bucket('portfolio.flowworkconsulting.com')
-build_bucket = s3.Bucket('portfoliobuild.flowworkconsulting.com')
+    #create names for the resource
+    s3 = boto3.resource('s3')
 
-#store object in memory
-portfolio_zip = StringIO.StringIO()
+    #create names for the buckets
+    portfolio_bucket = s3.Bucket('portfolio.flowworkconsulting.com')
+    build_bucket = s3.Bucket('portfoliobuild.flowworkconsulting.com')
 
-#download the zipfile to object in memory
-build_bucket.download_fileobj('portfoliobuild.zip',portfolio_zip)
+    #store object in memory
+    portfolio_zip = StringIO.StringIO()
 
-#extract the files, upload them and set the ACL permissions
-with zipfile.ZipFile(portfolio_zip) as myzip:
-    for nm in myzip.namelist():
-        obj = myzip.open(nm)
-        portfolio_bucket.upload_fileobj(obj, nm,
-        ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-        portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+    #download the zipfile to object in memory
+    build_bucket.download_fileobj('portfoliobuild.zip',portfolio_zip)
+
+    #extract the files, upload them and set the ACL permissions
+    with zipfile.ZipFile(portfolio_zip) as myzip:
+        for nm in myzip.namelist():
+            obj = myzip.open(nm)
+            portfolio_bucket.upload_fileobj(obj, nm,
+            ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+
+    print 'Job done!'
+    return 'Hello from Lambda'
