c1906a8ac92a9c1a393b8000ee62c96fa0d38a40
Jesus Colon <jfcp720@gmail.com>
2017-09-25 17:58:02 -0400

    fixing typos in lambda script

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -11,17 +11,30 @@ def lambda_handler(event, context):
     topic = sns.Topic('arn:aws:sns:us-east-1:671515529864:deployPortfolioTopic')
     s3 = boto3.resource('s3')
 
+    location = {
+        "bucketName": "portfolio.flowworkconsulting.com",
+        "objectKey": "portfoliobuild.zip"
+    }
 
     try:
+        #Initiate job object for CodePipeline
+        job = event.get("CodePipeline.job")
+
+        if job:
+            for artifact in job["data"]["inputArtifacts"]:
+                if artifact["name"]== "MyAppBuild":
+                    location = artifact["location"]["s3Location"]
+        print "Building portfolio from " + str(location)
+
         #create names for the buckets
         portfolio_bucket = s3.Bucket('portfolio.flowworkconsulting.com')
-        build_bucket = s3.Bucket('portfoliobuild.flowworkconsulting.com')
+        build_bucket = s3.Bucket(location["bucketName"])
 
         #store object in memory
         portfolio_zip = StringIO.StringIO()
 
         #download the zipfile to object in memory
-        build_bucket.download_fileobj('portfoliobuild.zip',portfolio_zip)
+        build_bucket.download_fileobj(location["objectKey"],portfolio_zip)
 
         #extract the files, upload them and set the ACL permissions
         with zipfile.ZipFile(portfolio_zip) as myzip:
@@ -33,6 +46,10 @@ def lambda_handler(event, context):
 
         print 'Job done!'
         topic.publish(Subject='Portfolio Deployed', Message='Portfolio deployed successfully.')
+        if job:
+            codepipeline = boto3.client('codepipeline')
+            codepipeline.put_job_success_result(jobId=job["id"])
+
     except:
         topic.publish(Subject="Portfolio Deploy Failed", Message='The Portfolio was not deployed successfully! :(')
         raise
